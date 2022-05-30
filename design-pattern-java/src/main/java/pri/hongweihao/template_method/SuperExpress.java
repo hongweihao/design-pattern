8891a1f98c74ee6d3424e5203b7cdbf38bb977c0
hongweihao <karlhong.weihao@qq.com>
2022-05-30 09:29:13 +0800

    fix: typo

--- design-pattern-java/src/main/java/pri/hongweihao/template_method/SuperExpress.java
+++ design-pattern-java/src/main/java/pri/hongweihao/template_method/SuperExpress.java
@@ -8,7 +8,7 @@ package pri.hongweihao.template_method;
  * @author Karl
  * @date 2022/5/25 18:04
  */
-public class SuperExpress extends AbstractExpressTemplate{
+public class SuperExpress extends AbstractExpressTemplate {
     @Override
     public void sendRequest() {
         System.out.println("App填写寄件单");
@@ -16,7 +16,7 @@ public class SuperExpress extends AbstractExpressTemplate{
 
     @Override
     public void pickUp() {
-        System.out.println("寄件员上面取件");
+        System.out.println("寄件员上门取件");
     }
 
     @Override
